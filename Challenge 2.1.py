80d99cfa5ee2dcbada8b3db791695abf4aa50980
Vmohan62 <145000782+Vmohan62@users.noreply.github.com>
2023-10-11 14:32:24 +0530

    Challenge 2.1.py

--- Challenge 2.1.py
+++ Challenge 2.1.py
@@ -1,17 +1,62 @@
-class Player:
-    def play(self):
-        print("The player is playing cricket.")
+class BankAccount:
 
-class Batsman(Player):
-    def play(self):
-        print("The batsman is batting.")
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
 
-class Bowler(Player):
-    def play(self):
-        print("The bowler is bowling.")
+        self.__account_number = account_number
 
-batsman = Batsman()
-bowler = Bowler()
+        self.__account_holder_name = account_holder_name
 
-batsman.play()
-bowler.play()
+        self.__account_balance = initial_balance
+
+
+
+    def deposit(self, amount):
+
+        if amount > 0:
+
+            self.__account_balance += amount
+
+            print(f"Deposited ${amount:.2f} into account {self.__account_number}")
+
+        else:
+
+            print("Invalid deposit amount. Please deposit a positive amount.")
+
+
+
+    def withdraw(self, amount):
+
+        if amount > 0:
+
+            if self.__account_balance >= amount:
+
+                self.__account_balance -= amount
+
+                print(f"Withdrew ${amount:.2f} from account {self.__account_number}")
+
+            else:
+
+                print("Insufficient balance. Cannot withdraw.")
+
+        else:
+
+            print("Invalid withdrawal amount. Please withdraw a positive amount.")
+
+
+
+    def display_balance(self):
+
+        print(f"Account {self.__account_number} balance: ${self.__account_balance:.2f}")
+
+
+if __name__== "__main__":
+
+    account1 = BankAccount("123456", "Darun", 1000.0)
+
+
+    account1.deposit(500.0)
+
+
+    account1.withdraw(200.0)
+
+    account1.display_balance()
